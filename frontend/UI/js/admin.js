fd600434d06254f449beb4c71e552d9b2b69f8c3
Nifemi <deyeminifemi@gmail.com>
2020-03-29 14:40:06 +0100

    Fetched users from API

--- frontend/UI/js/admin.js
+++ frontend/UI/js/admin.js
@@ -1,59 +1,61 @@
-const users = [
-    {
-        "amountDeposited": 0,
-        "currentBalance": 0,
-        "_id": "5e80619789cb6f3fa880d730",
-        "email": "dyeminifemi@gmail.com",
-        "password": "$2a$12$EUmJAooZ2ZisPEpsIZ.17eLEz8iE20RYeu0JA85ZddxqCDyM3GSs6",
-        "name": "Oluwanifemi Adeyemi",
-        "__v": 0
-    },
-    {
-        "amountDeposited": 30,
-        "currentBalance": 30,
-        "_id": "5e806352969a7b0b58744171",
-        "email": "deyeminifemi@gmail.com",
-        "password": "$2a$12$RR.n1Nx8Yo0V0gPxyBteEeTDhxwc6tI6rmNrHHMnTwUvpB7x9IrOS",
-        "name": "Oluwanifemi Adeyemi",
-        "__v": 0
-    },
-    {
-        "amountDeposited": 0,
-        "currentBalance": 0,
-        "_id": "5e80662c969a7b0b58744172",
-        "email": "yeminifemi@gmail.com",
-        "password": "$2a$12$dvklLQNj6/0cHEB4p0rBluDT5lcKIyVndb5JN2XR.pI.kWVOherb2",
-        "name": "Oluwanifemi Adeyemi",
-        "__v": 0
-    },
-    {
-        "amountDeposited": 0,
-        "currentBalance": 0,
-        "_id": "5e806679969a7b0b58744173",
-        "email": "eminifemi@gmail.com",
-        "password": "$2a$12$vx2gSoFp8gHfXUFmM3inru81E4Ue3RdsbBoddiYN3.Z3Vx9e1LfFC",
-        "name": "Oluwanifemi Adeyemi",
-        "__v": 0
-    },
-    {
-        "amountDeposited": 0,
-        "currentBalance": 0,
-        "_id": "5e806684969a7b0b58744174",
-        "email": "minifemi@gmail.com",
-        "password": "$2a$12$ks6EJX.vivRGWGaOzjvK4ejeCSQkvdWjLjv0Gf/zOGQ/MG33C3Z2C",
-        "name": "Oluwanifemi Adeyemi",
-        "__v": 0
-    },
-    {
-        "amountDeposited": 0,
-        "currentBalance": 0,
-        "_id": "5e806697969a7b0b58744175",
-        "email": "deyeminifemi@gmail.co",
-        "password": "$2a$12$k9.vfjuPSCR3qP.zstz.zuhxe0oVn2sA2r52.Guo0X7RQ9OaDJ2xO",
-        "name": "Oluwanifemi Adeyemi",
-        "__v": 0
-    }
-];
+// const users = [
+//     {
+//         "amountDeposited": 0,
+//         "currentBalance": 0,
+//         "_id": "5e80619789cb6f3fa880d730",
+//         "email": "dyeminifemi@gmail.com",
+//         "password": "$2a$12$EUmJAooZ2ZisPEpsIZ.17eLEz8iE20RYeu0JA85ZddxqCDyM3GSs6",
+//         "name": "Oluwanifemi Adeyemi",
+//         "__v": 0
+//     },
+//     {
+//         "amountDeposited": 30,
+//         "currentBalance": 30,
+//         "_id": "5e806352969a7b0b58744171",
+//         "email": "deyeminifemi@gmail.com",
+//         "password": "$2a$12$RR.n1Nx8Yo0V0gPxyBteEeTDhxwc6tI6rmNrHHMnTwUvpB7x9IrOS",
+//         "name": "Oluwanifemi Adeyemi",
+//         "__v": 0
+//     },
+//     {
+//         "amountDeposited": 0,
+//         "currentBalance": 0,
+//         "_id": "5e80662c969a7b0b58744172",
+//         "email": "yeminifemi@gmail.com",
+//         "password": "$2a$12$dvklLQNj6/0cHEB4p0rBluDT5lcKIyVndb5JN2XR.pI.kWVOherb2",
+//         "name": "Oluwanifemi Adeyemi",
+//         "__v": 0
+//     },
+//     {
+//         "amountDeposited": 0,
+//         "currentBalance": 0,
+//         "_id": "5e806679969a7b0b58744173",
+//         "email": "eminifemi@gmail.com",
+//         "password": "$2a$12$vx2gSoFp8gHfXUFmM3inru81E4Ue3RdsbBoddiYN3.Z3Vx9e1LfFC",
+//         "name": "Oluwanifemi Adeyemi",
+//         "__v": 0
+//     },
+//     {
+//         "amountDeposited": 0,
+//         "currentBalance": 0,
+//         "_id": "5e806684969a7b0b58744174",
+//         "email": "minifemi@gmail.com",
+//         "password": "$2a$12$ks6EJX.vivRGWGaOzjvK4ejeCSQkvdWjLjv0Gf/zOGQ/MG33C3Z2C",
+//         "name": "Oluwanifemi Adeyemi",
+//         "__v": 0
+//     },
+//     {
+//         "amountDeposited": 0,
+//         "currentBalance": 0,
+//         "_id": "5e806697969a7b0b58744175",
+//         "email": "deyeminifemi@gmail.co",
+//         "password": "$2a$12$k9.vfjuPSCR3qP.zstz.zuhxe0oVn2sA2r52.Guo0X7RQ9OaDJ2xO",
+//         "name": "Oluwanifemi Adeyemi",
+//         "__v": 0
+//     }
+// ];
+
+let users;
 
 const depositModal = document.getElementById('depositModal');
 const profitModal = document.getElementById('profitModal');
@@ -114,15 +116,30 @@ const getButtons = (id) => {
     return buttonsDiv
 };
 
-users.forEach(user => {
-    const div = getRow(user);
+window.fetchHelper('GET', 'all-users')
+    .then((response) => {
+        if(response.status === 200){
+            return JSON.parse(response.responseText)
+        }
+        return Promise.reject();
+    })
+    .then(({users: fetchedUsers}) => {
+        users = fetchedUsers;
+        users.forEach(user => {
+            const div = getRow(user);
 
-    const buttons = getButtons(user['_id']);
+            const buttons = getButtons(user['_id']);
 
-    div.appendChild(buttons);
+            div.appendChild(buttons);
 
-    document.querySelector('.table').appendChild(div);
-});
+            document.querySelector('.table').appendChild(div);
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        window.setStatus('error', true, 'There was an issue fetching the data. Kindly try again');
+    });
+
 
 depositModal.addEventListener('click', ({target}) => {
     if(depositModal.isEqualNode(target)){

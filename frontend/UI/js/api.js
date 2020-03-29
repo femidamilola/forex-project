3a1080433e3db36c5065f4b7eb3d7ced5dcf68ae
Nifemi <deyeminifemi@gmail.com>
2020-03-29 10:15:22 +0100

    Created api.js file with helpers for fetching

---
+++ frontend/UI/js/api.js
@@ -0,0 +1,30 @@
+const API_URL = 'http://www.localhost:3200';
+
+const makeRequest = async (method,url, body = null) => new Promise((resolve,reject) => {
+    const request = new XMLHttpRequest();
+
+    request.addEventListener('load',() => resolve(request));
+    request.addEventListener('error',() => reject(request));
+
+    request.open(method,url);
+
+    if(body){
+        request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    }
+
+    request.send(JSON.stringify(body));
+});
+
+window.fetchHelper = async (method, route, body = null) => {
+    try {
+        let response;
+        if(body){
+            response = await makeRequest(method,`${API_URL}/${route}`,body)
+        }else{
+            response = await makeRequest(method,`${API_URL}/${route}`)
+        }
+        return response
+    } catch (e) {
+        console.log(e);
+    }
+};

722d18e7ffc5015b29ff73b4521c1f6a75502b3c
Nifemi <deyeminifemi@gmail.com>
2020-03-29 13:05:24 +0100

    Filled user details based on reply from API

--- frontend/UI/js/user.js
+++ frontend/UI/js/user.js
@@ -1,13 +1,39 @@
 const user = localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')) : null;
 
-console.log(user)
-
 if (user) {
-    window.fetchHelper('GET', `dashboard/${user.email}`, null,[
+    window.fetchHelper('GET', `dashboard/${user.email}`, null, [
         {
             key: 'x-access-token',
             value: user.token
         }
     ])
+        .then((response) => {
+            if (response.status === 200) {
+                return JSON.parse(response.responseText)
+            }
+            return Promise.reject()
+        })
+        .then(user => {
+            const keys = [
+                'amountDeposited',
+                'currentBalance',
+                '_id',
+                'email',
+                'name',
+            ];
+            keys.forEach(key => {
+                document.getElementById(key).innerHTML = user[key]
+            });
+            if(user.currentBalance){
+                const increase = ((user.currentBalance - user.amountDeposited)/user.currentBalance) * 100
+                document.getElementById('increase').innerHTML = `${increase}%`
+            }else{
+                document.getElementById('increase').innerHTML = '0%'
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            window.setStatus('error', true, 'There was an error fetching your details. Kindly check your internet connection and try again.')
+        });
 }
 
